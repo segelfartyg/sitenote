f1aca399eacaccabf980232be65183065c252e5e
segelfartyg <samuel.swaren@outlook.com>
2024-06-15 18:41:02 +0200

    Add: initial db support

--- SiteNote.Backend/SiteNote.Server/go.mod
+++ SiteNote.Backend/SiteNote.Server/go.mod
@@ -11,9 +11,17 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.15.1 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.opentelemetry.io/otel v1.24.0 // indirect
@@ -22,6 +30,7 @@ require (
 	golang.org/x/crypto v0.24.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/api v0.184.0 // indirect
--- SiteNote.Backend/SiteNote.Server/main.go
+++ SiteNote.Backend/SiteNote.Server/main.go
@@ -6,18 +6,68 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"cloud.google.com/go/auth/credentials/idtoken"
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Audience string = "1010731658636-aeejci8n3gctj78iqdehtti3qfqpn568.apps.googleusercontent.com"
+
+var (
+	DB                *mongo.Client
+	userCollection    *mongo.Collection
+	findingCollection *mongo.Collection
+)
+
+type User struct {
+	UserId  string    `bson:"userId"`
+	Email   string    `bson:"email"`
+	Created time.Time `bson:"created"`
+}
+
+type Finding struct {
+	FindingId string    `bson:"findingId"`
+	Name      string    `bson:"name"`
+	Link      string    `bson:"link"`
+	UserId    string    `bson:"userId"`
+	Created   time.Time `bson:"created"`
+}
 
 type LoginRequest struct {
 	IdToken string `json:"id_token"`
 }
 
 func main() {
+
+	// SETTING UP DB CLIENT
+	fmt.Println("Connecting to DB")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	DB, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	userCollection = DB.Database("notelad").Collection("users")
+	findingCollection = DB.Database("notelad").Collection("findings")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if CheckIfUserExist("admin").UserId == "" {
+		CreateUser("admin", "admin@notelad.com")
+	}
+
+	if CheckIfFindingExist("1").FindingId == "" {
+		CreateFinding("1", "First Finding In NoteLad", "https://splodo.com", "admin")
+	}
 
 	fmt.Println("Starting server")
 
@@ -61,5 +111,78 @@ func ValidateGoogleIdToken(token string) {
 		log.Fatal(err)
 	}
 
+	sub := payload.Subject
+
+	if CheckIfUserExist(sub).UserId == "" {
+		CreateUser(sub, "no_email")
+	}
+
+	fmt.Print(sub)
 	fmt.Print(payload.Claims)
+	fmt.Print(payload.Claims)
+}
+
+func CheckIfUserExist(userId string) User {
+	var result User
+
+	filter := bson.D{{Key: "userId", Value: userId}}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	err := userCollection.FindOne(ctx, filter).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
+
+func CreateUser(userId string, email string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res, err := userCollection.InsertOne(ctx, bson.D{{Key: "userId", Value: userId}, {Key: "email", Value: email}, {Key: "created", Value: time.Now().UTC().UnixMilli()}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := res.InsertedID
+	fmt.Print(id)
+}
+
+func CreateFinding(findingId string, name string, link string, userId string) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	res, err := findingCollection.InsertOne(ctx, bson.D{{Key: "findingId", Value: findingId}, {Key: "name", Value: name}, {Key: "link", Value: link}, {Key: "userId", Value: userId}, {Key: "created", Value: time.Now().UTC().UnixMilli()}})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	id := res.InsertedID
+	fmt.Print(id)
+}
+
+func CheckIfFindingExist(findingId string) Finding {
+	var result Finding
+
+	filter := bson.D{{Key: "findingId", Value: findingId}}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	err := findingCollection.FindOne(ctx, filter).Decode(&result)
+
+	if err == mongo.ErrNoDocuments {
+		// Do something when no record was found
+		fmt.Println("record does not exist")
+	} else if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
 }
