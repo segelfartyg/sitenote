14791ec34b1cb0a8a3264853153e46e16fa13985
segelfartyg <samuel.swaren@outlook.com>
2024-07-23 12:35:48 +0200

    Add: webhook functions

--- deployment/webhook/main.go
+++ deployment/webhook/main.go
@@ -16,6 +16,53 @@ func main() {
 
 func deploy(c *gin.Context) {
 
+	composeDown()
+	removeImages()
+	getLatestImages()
+	composeUp()
+
+	res := "deployment succeeded"
+
+	c.String(http.StatusOK, res)
+}
+
+func composeDown() {
+
+	cmd := exec.Command("docker", "compose", "-f", "../dev/dev.docker-compose.yaml", "down")
+	commandOutput, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	successMessage := string(commandOutput)
+	fmt.Println(successMessage)
+
+}
+
+func removeImages() {
+	cmd := exec.Command("docker", "compose", "-f", "../dev/dev.docker-compose.yaml", "rm", "-f")
+	commandOutput, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	successMessage := string(commandOutput)
+	fmt.Println(successMessage)
+
+}
+
+func getLatestImages() {
+	cmd := exec.Command("docker", "compose", "-f", "../dev/dev.docker-compose.yaml", "pull", "-f")
+	commandOutput, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	successMessage := string(commandOutput)
+	fmt.Println(successMessage)
+}
+
+func composeUp() {
 	cmd := exec.Command("docker", "compose", "-f", "../dev/dev.docker-compose.yaml", "up", "-d")
 	commandOutput, err := cmd.Output()
 
@@ -24,6 +71,5 @@ func deploy(c *gin.Context) {
 		return
 	}
 	successMessage := string(commandOutput)
-
-	c.String(http.StatusOK, successMessage)
+	fmt.Println(successMessage)
 }
